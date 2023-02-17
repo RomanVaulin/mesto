febd28e0ed0b334dde62fc928767ced19af10a28
Roman Vaulin <vaulin007@yandex.ru>
2023-02-17 18:10:07 +0500

    init.com

---
+++ scripts/index.js
@@ -0,0 +1,40 @@
+const editButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const closeButton = popup.querySelector('.popup__close-button');
+
+const saveButton = popup.querySelector('.popup__container');
+
+const nameProfile = document.querySelector('.profile__name');
+const jobProfile = document.querySelector('.profile__description');
+const nameInput = popup.querySelector('.popup__field-name_input');
+const jobInput = popup.querySelector('.popup__field-description_input');
+
+const toggleOpenPopup = () => {
+    popup.classList.toggle('popup_opened');
+}
+const handleEditButtonClick = () => {
+    toggleOpenPopup();
+    nameInput.value = nameProfile.textContent.trim();
+    jobInput.value = jobProfile.textContent.trim();
+}
+const handleCloseButtonClick = () => {
+    toggleOpenPopup();
+}
+const handleOverlayClick = (event) => {
+    if (event.target === event.currentTarget) {
+        toggleOpenPopup();
+    }
+}
+
+function handleSaveButtonSubmit (evt) {
+    evt.preventDefault();
+    nameProfile.textContent = nameInput.value.trim();
+    jobProfile.textContent = jobInput.value.trim();
+
+    handleCloseButtonClick();
+}
+
+editButton.addEventListener('click', handleEditButtonClick);
+closeButton.addEventListener('click', handleCloseButtonClick);
+popup.addEventListener('click', handleOverlayClick);
+saveButton.addEventListener('submit', handleSaveButtonSubmit);
