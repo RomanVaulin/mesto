647d1a48f28f4fcb5995bb3e4d8e68533de48f3a
Roman Vaulin <vaulin007@yandex.ru>
2023-02-22 01:18:10 +0500

    fix_2

--- scripts/index.js
+++ scripts/index.js
@@ -2,31 +2,23 @@ const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closeButton = popup.querySelector('.popup__close-button');
 
-const saveButton = popup.querySelector('.popup__form');
+const submitButton = popup.querySelector('.popup__form');
 
 const nameProfile = document.querySelector('.profile__name');
 const jobProfile = document.querySelector('.profile__description');
 const nameInput = popup.querySelector('.popup__field_name_input');
 const jobInput = popup.querySelector('.popup__field_description_input');
 
-const toggleOpenPopup = () => {
-    popup.classList.toggle('popup_opened');
-}
 const handleEditButtonClick = () => {
-    toggleOpenPopup();
+    popup.classList.add('popup_opened');
     nameInput.value = nameProfile.textContent.trim();
     jobInput.value = jobProfile.textContent.trim();
 }
 const handleCloseButtonClick = () => {
-    toggleOpenPopup();
-}
-const handleOverlayClick = (event) => {
-    if (event.target === event.currentTarget) {
-        toggleOpenPopup();
-    }
+    popup.classList.remove('popup_opened');
 }
 
-function handleSaveButtonSubmit (evt) {
+function handleSubmitButtonSubmit (evt) {
     evt.preventDefault();
     nameProfile.textContent = nameInput.value.trim();
     jobProfile.textContent = jobInput.value.trim();
@@ -36,5 +28,4 @@ function handleSaveButtonSubmit (evt) {
 
 editButton.addEventListener('click', handleEditButtonClick);
 closeButton.addEventListener('click', handleCloseButtonClick);
-popup.addEventListener('click', handleOverlayClick);
-saveButton.addEventListener('submit', handleSaveButtonSubmit);
+submitButton.addEventListener('submit', handleSubmitButtonSubmit);
