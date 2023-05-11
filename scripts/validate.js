43502461fe2ac7499df90705f906f1263cb9d75c
Roman Vaulin <vaulin007@yandex.ru>
2023-05-12 01:17:27 +0500

    pr6_1.1

--- scripts/validate.js
+++ scripts/validate.js
@@ -3,7 +3,7 @@ const enableValidation = {
     inputSelector: '.popup__input',
     submitButtonSelector: '.popup__button',
     inactiveButtonClass: 'popup__button_disabled',
-    inputErrorClass: 'popup__input_type_error',
+    inputErrorClass: 'popup__input_type-error',
     errorClass: 'popup__error_visible'
 };
 
